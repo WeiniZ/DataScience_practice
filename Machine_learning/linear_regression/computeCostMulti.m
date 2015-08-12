6cdc5041038603f54e0207d3232544b1df9487c4
WeiniZ <weini.uaz@gmail.com>
2015-08-12 16:38:11 -0500

    Update compCostMulti.m

--- Machine_learning/linear_regression/computeCostMulti.m
+++ Machine_learning/linear_regression/computeCostMulti.m
@@ -13,7 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
+hx = (theta' * X')';
+J = 1/(2*m)* (hx - y)'*(hx - y);
 
 
 
